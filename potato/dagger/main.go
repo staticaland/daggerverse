505e0b302413b2e8c29b955310823134f4cd9b77
Anders K. Pettersen <staticaland@users.noreply.github.com>
2024-03-06 21:38:59 +0100

    feat: Multiple input parameters
    Some are optional.

--- potato/dagger/main.go
+++ potato/dagger/main.go
@@ -1,7 +1,19 @@
 package main
 
+import "fmt"
+
 type Potato struct{}
 
-func (m *Potato) HelloWorld() string {
-	return "Hello Daggernauts!"
+func (m *Potato) HelloWorld(
+	// the number of potatoes to process
+	count int,
+
+	// whether the potatoes are mashed
+	// +optional
+	mashed bool,
+) string {
+	if mashed {
+		return fmt.Sprintf("Hello Daggernauts, I have mashed %d potatoes", count)
+	}
+	return fmt.Sprintf("Hello Daggernauts, I have %d potatoes", count)
 }
